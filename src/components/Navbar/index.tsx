a1c83ffdfaffffe4e121d8d7b557ca44f53bf012
daffarafip24 <daffarafip24@gmail.com>
2025-02-19 15:07:04 +0700

    chore: Favicon

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Brain } from 'lucide-react'
+import { BrainCircuit } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
 import {
@@ -16,7 +16,7 @@ export const Navbar: React.FC = () => {
     <NavigationMenu className="absolute text-blue-100 top-0 box-border w-full max-w-auto left-0 px-4 lg:px-6 h-16 border-b border-blue-800/50">
       <div className="container mx-auto grid grid-cols-3 items-center justify-between ">
         <Link className="flex items-center justify-center  w-min" href="/">
-          <Brain className="h-8 w-8 text-blue-400" />
+          <BrainCircuit className="h-8 w-8 text-blue-400" />
           <span className="ml-2 text-2xl font-bold font-orbitron whitespace-nowrap">
             AI-Tribunal
           </span>
